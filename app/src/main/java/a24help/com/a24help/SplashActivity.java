27583af5158b36aa00d8b568e10af2137ce57175
waltson <waltson.silva@hotmail.com>
2019-01-19 16:27:27 -0300

    commit incial do projeto

---
+++ app/src/main/java/a24help/com/a24help/SplashActivity.java
@@ -0,0 +1,27 @@
+package a24help.com.a24help;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity implements Runnable {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(this, 3000);
+    }
+    public void run(){
+        startActivity(new Intent(this, LoginActivity.class));
+        finish();
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+}
---
+++ app/src/main/java/a24help/com/a24help/dao/ConfigureFirebase.java
@@ -0,0 +1,28 @@
+package a24help.com.a24help.dao;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+
+public class ConfigureFirebase {
+
+
+    private static DatabaseReference refenciaFirebase;
+    private static FirebaseAuth autenticacao;
+
+
+    public static DatabaseReference getFirebase() {
+        if (refenciaFirebase == null) {
+            refenciaFirebase = FirebaseDatabase.getInstance().getReference();
+        }
+        return refenciaFirebase;
+    }
+
+    public static FirebaseAuth getFirebaseAutenticacao() {
+        if (autenticacao == null) {
+            autenticacao = FirebaseAuth.getInstance();
+        }
+        return autenticacao;
+    }
+}
